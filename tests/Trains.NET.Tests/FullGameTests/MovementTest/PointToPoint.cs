eacde45e6f8b3a4b328dea9a0487afa07abb4e5c
Scott Holden <scott@nullops.io>
2020-03-12 23:00:36 +1100

    Added Crosses and Y Tracks

--- tests/Trains.NET.Tests/FullGameTests/MovementTest/PointToPoint.cs
+++ tests/Trains.NET.Tests/FullGameTests/MovementTest/PointToPoint.cs
@@ -268,5 +268,220 @@ namespace Trains.NET.Tests.FullGameTests.MovementTest
             Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
             Assert.Equal(expectedAngle, train.Angle, MovementPrecision);
         }
+
+        [Theory]
+        [InlineData(2, 1, 90.0f, 2, 3)] // Down
+        [InlineData(2, 3, 270.0f, 2, 1)] // Up
+        [InlineData(1, 2, 0.0f, 3, 2)] // Right
+        [InlineData(3, 2, 180.0f, 1, 2)] // Left
+        public void MovementTest_PointToPoint_HorizontalVertical_Cross_HorizontalVertical(int startingColumn, int startingRow, float angle, int expectedColumn, int expectedRow)
+        {
+            var board = new GameBoard(false);
+
+            board.AddTrack(2, 1); // Vertical
+            board.AddTrack(1, 2); // Horizontal
+            board.AddTrack(2, 2); // Cross
+            board.AddTrack(3, 2); // Horizontal
+            board.AddTrack(2, 3); // Vertical
+
+            board.AddTrain(startingColumn, startingRow);
+
+            Train train = board.GetTrains().Single();
+            train.Angle = angle;
+
+            Assert.Equal(startingColumn, train.Column);
+            Assert.Equal(startingRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(angle, train.Angle, MovementPrecision);
+
+            float distance = 2.0f;
+
+            // Move it!
+            for (int i = 0; i < _movementSteps; i++)
+                board.GameLoopStep(distance / _movementSteps);
+
+            Assert.Equal(expectedColumn, train.Column);
+            Assert.Equal(expectedRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(angle, train.Angle, MovementPrecision);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 0.0f, 2, 3, 90.0f)] // Left to Down
+        [InlineData(2, 1, 90.0f, 1, 2, 180.0f)] // Up to Left
+        [InlineData(2, 3, 270.0f, 1, 2, 180.0f)] // Down to Left
+        public void MovementTest_PointToPoint_Horizontal_LeftUpDown_VerticalVertical(int startingColumn, int startingRow, float startingAngle, int expectedColumn, int expectedRow, float expectedAngle)
+        {
+            var board = new GameBoard(false);
+
+            board.AddTrack(1, 2); // Horizontal
+            board.AddTrack(2, 1); // Vertical
+            board.AddTrack(2, 3); // Vertical
+            board.AddTrack(2, 2); // Y Track
+
+            board.AddTrain(startingColumn, startingRow);
+
+            Train train = board.GetTrains().Single();
+            train.Angle = startingAngle;
+
+            Assert.Equal(startingColumn, train.Column);
+            Assert.Equal(startingRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(startingAngle, train.Angle, MovementPrecision);
+
+            // Traveling from middle of 1 straight + curve + to middle of 2nd straight
+            //  This assumes that track radius is 0.5f
+            //float distance = 0.5f + (float)Math.PI / 4.0f + 0.5f;
+
+            float radius = 0.5f;
+            float circleCircumference = 2.0f * (float)Math.PI * radius;
+
+            float distance = 0.5f + circleCircumference / 4.0f + 0.5f;
+
+            // Move it!
+            for (int i = 0; i < _movementSteps; i++)
+                board.GameLoopStep(distance / _movementSteps);
+
+            Assert.Equal(expectedColumn, train.Column);
+            Assert.Equal(expectedRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(expectedAngle, train.Angle, MovementPrecision);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 180.0f, 1, 1, 270.0f)] // Right to Up
+        [InlineData(1, 1, 90.0f, 2, 2, 0.0f)] // Up to Right
+        [InlineData(1, 3, 270.0f, 2, 2, 0.0f)] // Down to Right
+        public void MovementTest_PointToPoint_VerticalVertical_RightUpDown_Horizontal(int startingColumn, int startingRow, float startingAngle, int expectedColumn, int expectedRow, float expectedAngle)
+        {
+            var board = new GameBoard(false);
+
+            board.AddTrack(1, 1); // Vertical
+            board.AddTrack(1, 3); // Vertical
+            board.AddTrack(2, 2); // Horizontal
+            board.AddTrack(1, 2); // Y Track
+
+            board.AddTrain(startingColumn, startingRow);
+
+            Train train = board.GetTrains().Single();
+            train.Angle = startingAngle;
+
+            Assert.Equal(startingColumn, train.Column);
+            Assert.Equal(startingRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(startingAngle, train.Angle, MovementPrecision);
+
+            // Traveling from middle of 1 straight + curve + to middle of 2nd straight
+            //  This assumes that track radius is 0.5f
+            //float distance = 0.5f + (float)Math.PI / 4.0f + 0.5f;
+
+            float radius = 0.5f;
+            float circleCircumference = 2.0f * (float)Math.PI * radius;
+
+            float distance = 0.5f + circleCircumference / 4.0f + 0.5f;
+
+            // Move it!
+            for (int i = 0; i < _movementSteps; i++)
+                board.GameLoopStep(distance / _movementSteps);
+
+            Assert.Equal(expectedColumn, train.Column);
+            Assert.Equal(expectedRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(expectedAngle, train.Angle, MovementPrecision);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 0.0f, 2, 2, 90.0f)] // Left to Down
+        [InlineData(2, 2, 270.0f, 3, 1, 0.0f)] // Down to Left
+        [InlineData(3, 1, 180.0f, 2, 2, 90.0f)] // Right to Down
+        public void MovementTest_PointToPoint_HorizontalHorizontal_LeftRightDown_Vertical(int startingColumn, int startingRow, float startingAngle, int expectedColumn, int expectedRow, float expectedAngle)
+        {
+            var board = new GameBoard(false);
+
+            board.AddTrack(1, 1); // Horizontal
+            board.AddTrack(3, 1); // Horizontal
+            board.AddTrack(2, 2); // Vertical
+            board.AddTrack(2, 1); // Y Track
+            
+            board.AddTrain(startingColumn, startingRow);
+
+            Train train = board.GetTrains().Single();
+            train.Angle = startingAngle;
+
+            Assert.Equal(startingColumn, train.Column);
+            Assert.Equal(startingRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(startingAngle, train.Angle, MovementPrecision);
+
+            // Traveling from middle of 1 straight + curve + to middle of 2nd straight
+            //  This assumes that track radius is 0.5f
+            //float distance = 0.5f + (float)Math.PI / 4.0f + 0.5f;
+
+            float radius = 0.5f;
+            float circleCircumference = 2.0f * (float)Math.PI * radius;
+
+            float distance = 0.5f + circleCircumference / 4.0f + 0.5f;
+
+            // Move it!
+            for (int i = 0; i < _movementSteps; i++)
+                board.GameLoopStep(distance / _movementSteps);
+
+            Assert.Equal(expectedColumn, train.Column);
+            Assert.Equal(expectedRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(expectedAngle, train.Angle, MovementPrecision);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 0.0f, 2, 1, 270.0f)] // Left to Up
+        [InlineData(2, 1, 90.0f, 1, 2, 180.0f)] // Up to Left
+        [InlineData(3, 2, 180.0f, 2, 1, 270.0f)] // Right to Up
+        public void MovementTest_PointToPoint_HorizontalHorizontal_LeftRightUp_Vertical(int startingColumn, int startingRow, float startingAngle, int expectedColumn, int expectedRow, float expectedAngle)
+        {
+            var board = new GameBoard(false);
+
+            board.AddTrack(1, 2); // Horizontal
+            board.AddTrack(3, 2); // Horizontal
+            board.AddTrack(2, 1); // Vertical
+            board.AddTrack(2, 2); // Y Track
+
+            board.AddTrain(startingColumn, startingRow);
+
+            Train train = board.GetTrains().Single();
+            train.Angle = startingAngle;
+
+            Assert.Equal(startingColumn, train.Column);
+            Assert.Equal(startingRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(startingAngle, train.Angle, MovementPrecision);
+
+            // Traveling from middle of 1 straight + curve + to middle of 2nd straight
+            //  This assumes that track radius is 0.5f
+            //float distance = 0.5f + (float)Math.PI / 4.0f + 0.5f;
+
+            float radius = 0.5f;
+            float circleCircumference = 2.0f * (float)Math.PI * radius;
+
+            float distance = 0.5f + circleCircumference / 4.0f + 0.5f;
+
+            // Move it!
+            for (int i = 0; i < _movementSteps; i++)
+                board.GameLoopStep(distance / _movementSteps);
+
+            Assert.Equal(expectedColumn, train.Column);
+            Assert.Equal(expectedRow, train.Row);
+            Assert.Equal(0.5f, train.RelativeLeft, MovementPrecision);
+            Assert.Equal(0.5f, train.RelativeTop, MovementPrecision);
+            Assert.Equal(expectedAngle, train.Angle, MovementPrecision);
+        }
     }
 }
